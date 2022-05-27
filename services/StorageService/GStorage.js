3fd36596eeb0879a5d793135388db28a680a4d1a
asif <asif07c@gmail.com>
2022-05-27 15:36:15 +0600

    localstorage and gcp storage provider classes

---
+++ services/StorageService/GStorage.js
@@ -0,0 +1,67 @@
+const { Storage } = require('@google-cloud/storage');
+const got = require('got');
+
+require('dotenv').config();
+
+const storage = new Storage({
+    projectId: process.env.GCLOUD_PROJECT,
+    credentials: {
+        client_email: process.env.GCLOUD_CLIENT_EMAIL,
+        private_key: process.env.GCLOUD_PRIVATE_KEY
+    }
+});
+
+class GStorage {
+    constructor() {
+        this._name = 'google';
+        this._bucket = storage.bucket(process.env.GCS_BUCKET);
+        this._bucket_url = `https://storage.googleapis.com/${process.env.GCS_BUCKET}`;
+    }
+
+    async upload(file) {
+
+        const uploadFileToGCStorage = (file) => {
+            return new Promise((resolve, reject) => {
+                const { newfilename } = file;
+                const blob = this._bucket.file(newfilename);
+                const blobStream = blob.createWriteStream();
+
+                blobStream.on('error', error => {
+                    reject(error);
+                });
+
+                blobStream.on('finish', () => {
+                    resolve(true);
+                });
+
+                blobStream.end(file.buffer);
+            });
+        }
+
+        try {
+            await uploadFileToGCStorage(file);
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+
+    read(fileName) {
+        try {
+            return got.stream(`${this._bucket_url}/${fileName}`);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async delete(fileName) {
+        try {
+            await storage.bucket(process.env.GCS_BUCKET).file(fileName).delete();
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+}
+
+module.exports = GStorage;
---
+++ services/StorageService/LocalStorage.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const dotEnv = require('dotenv');
+dotEnv.config();
+
+const fileHandler = fs.promises;
+class LocalStorage {
+    constructor() {
+        this._name = 'local';
+        this._bucket = process.env.FOLDER;
+    }
+
+    async upload(file) {
+        try {
+            // console.log("location: ", this._bucket)
+            await fileHandler.writeFile(`${this._bucket}/${file.newfilename}`, file.buffer);
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+
+    read(fileName) {
+        try {
+            return fs.createReadStream(`${this._bucket}/${fileName}`);
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+
+    async delete(fileName) {
+        try {
+            await fileHandler.unlink(`${this._bucket}/${fileName}`);
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+}
+
+module.exports = LocalStorage;
