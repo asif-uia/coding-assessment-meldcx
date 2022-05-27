a7cab5c99ba4dd46f897dfe869aaa7734e801b43
asif <asif07c@gmail.com>
2022-05-27 23:43:27 +0600

    set database in config.test

--- config/config.test.js
+++ config/config.test.js
@@ -2,7 +2,7 @@
 
 const config = require("./config.defaults")
 
-config.db.host = "/home/Drive-A/coding_assessment_meldcx/db/test"
-config.db.dbName = "testdb.json"
+config.db.host = "/home/Drive-A/coding_assessment_meldcx/db"
+config.db.dbName = "datastore.json"
 
 module.exports = config
