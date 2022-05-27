2b7a2981644f5d5b39ea8e4fa70d8161cbcb50d5
asif <asif07c@gmail.com>
2022-05-27 15:35:37 +0600

    binder types added

---
+++ ioc/types.js
@@ -0,0 +1,12 @@
+const TYPES = {
+    StorageService: Symbol('StorageService'),
+    DbService: Symbol('DbService'),
+    CryptoService: Symbol('CryptoService'),
+    CryptoLength: Symbol('CryptoLength'),
+    CronService: Symbol('CronService'),
+    FileManager: Symbol('FileManager'),
+    FileManagerInstance: Symbol('FileManagerInstance'),
+    FileController: Symbol('FileController')
+}
+
+module.exports = TYPES;
